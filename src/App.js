aa9315e3b3f4294875ff7c49ff69454d979e861e
Olávio Timóteo <olaviotimoteo@hotmail.com>
2023-05-30 13:43:58 -0300

    feat: Ajusta layout e finaliza projeto

--- src/App.js
+++ src/App.js
@@ -96,25 +96,25 @@ const App = () => {
           <Button label="x" onClick={handleMultNumbers} />
           <Button label="/" onClick={handleDivNumbers} />
           <Button label="c" onClick={handleOnClear} />
-          <Button label="." />
-        </Row>
-        <Row>
-          <Button label="7" onClick={() => handleAddNumber("7")} />
-          <Button label="8" onClick={() => handleAddNumber("8")} />
-          <Button label="9" onClick={() => handleAddNumber("9")} />
           <Button label="-" onClick={handleMinusNumbers} />
-        </Row>
-        <Row>
-          <Button label="4" onClick={() => handleAddNumber("4")} />
-          <Button label="5" onClick={() => handleAddNumber("5")} />
-          <Button label="6" onClick={() => handleAddNumber("6")} />
-          <Button label="+" onClick={handleSumNumbers} />
         </Row>
         <Row>
           <Button label="1" onClick={() => handleAddNumber("1")} />
           <Button label="2" onClick={() => handleAddNumber("2")} />
           <Button label="3" onClick={() => handleAddNumber("3")} />
+          <Button label="+" onClick={handleSumNumbers} />
+        </Row>
+        <Row>
+          <Button label="4" onClick={() => handleAddNumber("4")} />
+          <Button label="5" onClick={() => handleAddNumber("5")} />
+          <Button label="6" onClick={() => handleAddNumber("6")} />
           <Button label="=" onClick={handleEquals} />
+        </Row>
+        <Row>
+          <Button label="7" onClick={() => handleAddNumber("7")} />
+          <Button label="8" onClick={() => handleAddNumber("8")} />
+          <Button label="9" onClick={() => handleAddNumber("9")} />
+          <Button label="0" onClick={() => handleAddNumber("0")} />
         </Row>
       </Content>
     </Container>
